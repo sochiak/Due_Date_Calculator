42d09ec5ff54fd9a3048ab573b9122856c4ec910
Sophia Herrell <43304686+sochiak@users.noreply.github.com>
2024-01-29 14:58:07 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function submitForm() {
+    //alert("functional");
+}
+
+// function handleCheckboxChange(inputId) {
+//     var inputElement = document.getElementById(inputId);
+//     var checkboxId = "unknown" + inputId.charAt(0).toUpperCase() + inputId.slice(1);
+//     var checkboxElement = document.getElementById(checkboxId);
+
+//     if (checkboxElement.checked) {
+//         inputElement.disabled = true;
+
+//         updateUrlParameter(inputId, 'X');
+//     } else {
+//         inputElement.disabled = false;
+
+//         updateUrlParameter(inputId, inputElement.value);
+//     }
+// }
+
+// function updateUrlParameter(param, value) {
+//     console.log(param + "=" + value);
+// }
+
