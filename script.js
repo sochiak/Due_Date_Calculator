9fa9f0930dd9a2e5459d0a151070607ab1ca58de
Sophia Herrell <43304686+sochiak@users.noreply.github.com>
2024-01-29 16:01:38 -0500

    Fixed autosubmit issue with form

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+// function submitForm() {
+//     alert("functional"); // just a test
+// }
+function handleCheckboxChange(inputId) {
+    var inputElement = document.getElementById(inputId);
+    var checkboxId = "unknown" + inputId.charAt(0).toUpperCase() + inputId.slice(1);
+    var checkboxElement = document.getElementById(checkboxId);
+
+    if (checkboxElement.checked) {
+        inputElement.disabled = true;
+    } else {
+        inputElement.disabled = false;
+    }
+}
+
 function submitForm() {
-    //alert("functional");
+    // Get input values
+
+    
 }
 
 // function handleCheckboxChange(inputId) {
